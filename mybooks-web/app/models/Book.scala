fc22f02d264abc50ac6a32ab82d6cb50970ed8bb
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-10-01 06:06:15 +0900

    本の情報を一冊出してみる

--- mybooks-web/app/models/Book.scala
+++ mybooks-web/app/models/Book.scala
@@ -4,7 +4,7 @@ import java.net.URL
 
 case class Book(
     title:String,
-    description:String = "本の説明がここに入ります",
-    link:URL = new URL("http://www.yahoo.co.jp"),
-    imageUrl:URL = new URL("http://k.yimg.jp/images/top/sp2/cmn/logo-ns-131205.png")
+    description:String,
+    link:URL,
+    imageUrl:URL
 )
