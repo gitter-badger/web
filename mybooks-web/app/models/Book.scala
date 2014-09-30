3963684f55d249a1bdf66efa48d4c2f0f24f59df
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-10-01 05:49:10 +0900

    本の情報を表示

--- mybooks-web/app/models/Book.scala
+++ mybooks-web/app/models/Book.scala
@@ -1,3 +1,10 @@
 package models
 
-case class Book(title:String)
+import java.net.URL
+
+case class Book(
+    title:String,
+    description:String = "本の説明がここに入ります",
+    link:URL = new URL("http://www.yahoo.co.jp"),
+    imageUrl:URL = new URL("http://k.yimg.jp/images/top/sp2/cmn/logo-ns-131205.png")
+)
