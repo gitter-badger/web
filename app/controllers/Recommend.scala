f72787539724f0702b5ede76bb2cbf9a8661fecc
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-10-04 23:58:34 +0900

    jsonファイルからコンテンツを読み込み

--- app/controllers/Recommend.scala
+++ app/controllers/Recommend.scala
@@ -5,16 +5,23 @@ import play.api.mvc._
 import models._
 import java.net.URL
 import java.util.Date
+import scala.io.Source
+import play.api.libs.json.Json
 
 object Recommend extends Controller {
 
     def today = Action {
+        //jsonの読み込み
+        //TODO model側に移す
+        def jsonString = Source.fromFile("./app/assets/recommends.json").mkString
+        def json = Json.parse(jsonString)
+
         Ok(views.html.recommend(
             Book(
-                title = "仕事は楽しいかね",
-                description = "本書は、将来への希望もなく日々仕事に追われる主人公が、老人のアドバイスに自己変革のアイデアを見いだしていく物語である。それは、唐突に繰り出される老人の言葉とそれを問いただす「私」の会話で展開していく。たとえば老人は「目標を立てるな」という。",
-                link = new URL("http://amazon.co.jp/o/ASIN/4877710787/shoma2da-22"),
-                imageUrl = new URL("http://ecx.images-amazon.com/images/I/717z8keSY7L.jpg")
+                title = (json \ "default" \ "title").as[String],
+                description = ((json \ "default" \ "description").as[String]),
+                link = new URL((json \ "default" \ "link").as[String]),
+                imageUrl = new URL((json \ "default" \ "imageUrl").as[String])
             ),
             new Date()
         ))
