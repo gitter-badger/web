adaf2993796b38052e228397861eeb1293fc4f8c
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-10-05 00:01:52 +0900

    ファイルの読み込みをモデルクラス側に移動

--- app/controllers/Recommend.scala
+++ app/controllers/Recommend.scala
@@ -5,26 +5,11 @@ import play.api.mvc._
 import models._
 import java.net.URL
 import java.util.Date
-import scala.io.Source
-import play.api.libs.json.Json
 
 object Recommend extends Controller {
 
     def today = Action {
-        //jsonの読み込み
-        //TODO model側に移す
-        def jsonString = Source.fromFile("./app/assets/recommends.json").mkString
-        def json = Json.parse(jsonString)
-
-        Ok(views.html.recommend(
-            Book(
-                title = (json \ "default" \ "title").as[String],
-                description = ((json \ "default" \ "description").as[String]),
-                link = new URL((json \ "default" \ "link").as[String]),
-                imageUrl = new URL((json \ "default" \ "imageUrl").as[String])
-            ),
-            new Date()
-        ))
+        Ok(views.html.recommend(Book.load, new Date()))
     }
 
 }
--- app/models/Book.scala
+++ app/models/Book.scala
@@ -1,6 +1,8 @@
 package models
 
 import java.net.URL
+import play.api.libs.json.Json
+import scala.io.Source
 
 case class Book(
     title:String,
@@ -8,3 +10,16 @@ case class Book(
     link:URL,
     imageUrl:URL
 )
+
+object Book {
+    def load = {
+        def jsonString = Source.fromFile("./app/assets/recommends.json").mkString
+        def json = Json.parse(jsonString)
+        Book(
+            title = (json \ "default" \ "title").as[String],
+            description = ((json \ "default" \ "description").as[String]),
+            link = new URL((json \ "default" \ "link").as[String]),
+            imageUrl = new URL((json \ "default" \ "imageUrl").as[String])
+        )
+    }
+}
